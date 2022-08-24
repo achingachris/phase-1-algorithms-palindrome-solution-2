49dcccb073a2bb1ad6e58c75c3bba39553769245
chrisislearning <chris.achinga@student.moringaschool.com>
2022-08-24 13:48:45 +0300

    not-working

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let re = /[^A-Za-z0-9]/g
+  str = str.toLowerCase().replace(re, '')
+  var len = str.length
+  for (var i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false
+    }
+  }
+  return true
 }
 
 /* 
@@ -13,13 +22,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log('Expecting: true')
+  console.log('=>', isPalindrome('racecar'))
 
-  console.log("");
+  console.log('')
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log('Expecting: false')
+  console.log('=>', isPalindrome('robot'))
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome
